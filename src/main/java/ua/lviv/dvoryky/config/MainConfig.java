0509515a5f40dd7f53c0644b564947c0502355f8
Nikita <nikiforovpizza@gmail.com>
2018-04-23 01:34:11 +0300

    Comment config

--- src/main/java/ua/lviv/dvoryky/config/MainConfig.java
+++ src/main/java/ua/lviv/dvoryky/config/MainConfig.java
@@ -10,19 +10,19 @@ import java.net.URISyntaxException;
 
 @Configuration
 public class MainConfig {
-    @Bean
-    public DataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-
-        DriverManagerDataSource basicDataSource = new DriverManagerDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
-    }
+//    @Bean
+//    public DataSource dataSource() throws URISyntaxException {
+//        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+//
+//        String username = dbUri.getUserInfo().split(":")[0];
+//        String password = dbUri.getUserInfo().split(":")[1];
+//        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+//
+//        DriverManagerDataSource basicDataSource = new DriverManagerDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
+//    }
 }
